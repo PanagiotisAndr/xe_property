cd89f638bd10abffc03e1fbae0cf63ea65994875
Takis Andreou <takandre2@gmail.com>
2023-10-19 14:31:59 +0300

    bootstrap 5 - Lodash

--- front/src/App.js
+++ front/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/Home';
 import PropertyAdPage from './pages/PropertyAdPage';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
